796ce3a504b00327173dda0cc9fb0d81c69a11be
Accelerator6869 <2770726488@qq.com>
2025-02-15 21:07:52 +0800

    更新

--- test_2_15.cpp
+++ test_2_15.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: Accelerator6869 2770726488@qq.com
+ * @Date: 2025-02-15 20:55:22
+ * @LastEditors: Accelerator6869 2770726488@qq.com
+ * @LastEditTime: 2025-02-15 20:57:58
+ * @FilePath: \frontend_practice\test_2_15.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include<iostream>
 using namespace std;
 
